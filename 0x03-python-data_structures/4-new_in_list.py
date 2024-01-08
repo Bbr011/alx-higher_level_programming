3c9c5af8a1ec7c1b237965885ea62ec716e3b83e
root <Bbr011>
2024-01-08 14:29:22 -0800

    replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    tempo_list = my_list[]
+    tempo_list = my_list[:]
     if 0 <= idx < len(my_list):
         tempo_list[idx] = element
-        return (my_list)
-    return (tempo_list)
+        return (tempo_list)
+    return (my_list)
