1b6b22305fffd35fde13d0318149b78419328374
root <Bbr011>
2024-01-08 13:44:51 -0800

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
-        return (mylist[idx] = element)
-    else:
-        return (my_list[])
+        mylist[idx] = element
+        return (my_list)
