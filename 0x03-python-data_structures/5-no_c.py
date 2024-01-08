27d54138d143f3c4a6f6d0e5c60a8cb0ea5206fd
root <Bbr011>
2024-01-08 14:37:03 -0800

    c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    for i in my_string:
-        if my_string[i] != c:
+    a = len(my_string)
+    for i in a:
+        if my_string[i] != "c":
             print(f"{my_string[i]}")
