3b039be35c484a07f7e8e8f8f10c2d46c2600a29
root <Bbr011>
2024-01-08 16:14:04 -0800

    bool

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     newlist = my_list[:]
-    for i, j in enumerate(my_list):
+    for j, i in enumerate(my_list):
         if i % 2 == 0:
             newlist[j] = True
         else:
