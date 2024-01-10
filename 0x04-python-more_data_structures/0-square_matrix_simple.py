1e741b7fa84449e32be57f21d1e7bf422b3e236a
root <Bbr011>
2024-01-10 12:32:07 -0800

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squar_matrix = matrix[:]
+    squar_matrix = matrix[:][:]
     for i in matrix:
         for j in matrix:
             squar_matrix[i][j] = matrix[i][j] ** 2
