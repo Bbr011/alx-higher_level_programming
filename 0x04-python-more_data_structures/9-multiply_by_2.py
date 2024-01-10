0a160ee3b2bb8ddd5f62fb65b11ecfb07c968b3b
root <Bbr011>
2024-01-10 15:21:31 -0800

    multi

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] = a_dictionary[i] * 2
+    for key, value in a_dictionary.items():
+        key: value * 2
+        
     return (a_dictionary)
