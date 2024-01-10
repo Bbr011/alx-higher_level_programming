dce845dd2c94456d6ae1f99f5f06b49c3e47f29b
root <Bbr011>
2024-01-10 13:22:17 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in my_list:
-        new_list[my_list.index(search)] = replace
-        return map(new_list)
-    return map(my_list)
+    return list(map(lambda i: replace if i == search else e, my_list))
