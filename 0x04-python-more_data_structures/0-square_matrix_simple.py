08dbb50da1439228ae438f77aa68e9b7a551ffa9
root <Bbr011>
2024-01-10 12:41:51 -0800

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda squar_matrix: list(map(lambda i: i**2, squar_matrix)),matrix) 
+    return (list(map(lambda squar_matrix: list(map(lambda i: i**2, squar_matrix)),matrix)))
