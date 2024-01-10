afc19587157bd4c94c036a7ce81266fe70f73c42
root <Bbr011>
2024-01-10 13:04:20 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i in rang(len(my_list)):
+    for i in range(len(my_list)):
         new_list(my_list.index(search)) = replace
         return (new_list)
     return (my_list)
