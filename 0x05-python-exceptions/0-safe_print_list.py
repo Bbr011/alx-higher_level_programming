c4247622fee4a58ef2a53b8b662a6643f00e46e6
root <Bbr011>
2024-01-22 13:07:09 -0800

    listex

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for i in range(my_list):
+        try:
+            print("{}".format(mylist[i]), end="")
+            j += 1
+        except Indexerror:
+            break
+    return (j)
