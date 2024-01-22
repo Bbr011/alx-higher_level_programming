c4c807d6a19986c0fd907f6942906f7c9bd1b7d6
root <Bbr011>
2024-01-22 14:46:02 -0800

    div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         n = x / y
-        return (n)
     except ZeroDivisionError:
-        return (None)
+        n = None
     finally:
         print("Inside result: {}".format(n))
+    return (n)
