5ad88c8f578b00948eb07cf760d53381110a5268
root <Bbr011>
2024-01-22 13:10:22 -0800

    listex

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     j = 0
     for i in range(my_list):
         try:
-            print("{}".format(mylist[i]), end="")
+            print("{}".format(my_list[i]), end="")
             j += 1
         except IndexError:
             break
