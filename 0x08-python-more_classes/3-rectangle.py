070d749eb6511610615815ec133c463cc66d23c2
root <Bbr011>
2024-01-30 15:11:21 -0800

    rectangle3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
 
         reclist = []
         for i in range(self.__height):
-                [reclist.append('#') for j in range(self.__width)]
-                if i != self.__height - 1:
-                    reclist.append("\n")
+            [reclist.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                reclist.append("\n")
         return ("".join(reclist))
