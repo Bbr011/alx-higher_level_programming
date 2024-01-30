cfad87e0b946e5ce36904db92db81666b2243350
root <Bbr011>
2024-01-30 13:50:33 -0800

    rectangle1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""class rectangle"""
+
+
+class Rectangle:
+    """rectangle"""
+    def __init__(self, width=0, height=0):
+        """initiaze new rectangle
+        Args:
+           width: width of rectangle
+           height: height of rectangle
+        """
+    @property
+    def width(self):
+        """retrieve the width of rectangle"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve the height of rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
