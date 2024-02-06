9e50bfe4fba691e3a771781d46befbc6a84b7b44
root <Bbr011>
2024-02-06 12:40:52 -0800

    inherites from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,6 @@ def inherits_from(obj, a_class):
     Returns:
         True if obj is an instance of a subclass of a_class, otherwise False.
     """
-    return isinstance(obj, type) and issubclass(obj, a_class) and obj != a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
