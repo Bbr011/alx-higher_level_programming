05380fc37f797ce7a1e836f9229bf6b3144db9bc
root <Bbr011>
2024-02-06 12:37:10 -0800

    inherites from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,6 +15,10 @@ def is_kind_of_class(obj, a_class):
     Returns:
     bool: True if obj is an instance of a_class or its subclass, otherwise False.
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
+     if type(obj) is a_class:
+        return False
+
+    for base in obj.__class__.__bases__:
+        if base is a_class or inherits_from(base, a_class):
+            return True
     return False
