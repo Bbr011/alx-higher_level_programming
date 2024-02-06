682693e871898515d4ea317e9c239dda1ce9d0bf
root <Bbr011>
2024-02-06 12:05:17 -0800

    sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,7 @@
 """class MyList that inherits from list"""
 class MyList(list):
     """Implements sorted printing for the built-in list class"""
+
     def print_sorted(self):
         """prints the list, but sorted"""
         print(sorted(self))
