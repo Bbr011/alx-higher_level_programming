1b8be32c789b9c7ab16ef60f64c8875fa964c6fe
root <Bbr011>
2024-02-06 12:26:06 -0800

    is instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class-checking function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class
+
+    Parameters:
+    obj: object
+        the object to check
+    a_class: class
+        class to check agains
+
+    Returns:
+    bool: True if obj is an instance of a_class or its subclass, otherwise False.
+    """
+    return isinstance(obj, a_class)
