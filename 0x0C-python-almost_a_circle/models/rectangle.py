1869c170a170e192e56199756f663a79259cf203
Bbr011 <khadraouih7@gmail.com>
2024-02-15 10:57:59 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,42 +21,38 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-    @property
-    def width(self):
-        '''Width of this rectangle.'''
-        return self.__width
+        @property
+        def width(self):
+            '''Width of this rectangle.'''
+            return self.__width
 
-    @width.setter
-    def width(self, value):
-        self.validate_integer("width", value, False)
-        self.__width = value
+        @width.setter
+        def width(self, value):
+            self.__width = value
 
-    @property
-    def height(self):
-        '''Height of this rectangle.'''
-        return self.__height
+        @property
+        def height(self):
+            '''Height of this rectangle.'''
+            return self.__height
 
-    @height.setter
-    def height(self, value):
-        self.validate_integer("height", value, False)
-        self.__height = value
+        @height.setter
+        def height(self, value):
+            self.__height = value
 
-    @property
-    def x(self):
-        '''x of this rectangle.'''
-        return self.__x
+        @property
+        def x(self):
+            '''x of this rectangle.'''
+            return self.__x
 
-    @x.setter
-    def x(self, value):
-        self.validate_integer("x", value)
-        self.__x = value
+        @x.setter
+        def x(self, value):
+            self.__x = value
+        
+        @property
+        def y(self):
+            '''y of this rectangle.'''
+            return self.__y
 
-    @property
-    def y(self):
-        '''y of this rectangle.'''
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        self.validate_integer("y", value)
-        self.__y = value
+        @y.setter
+        def y(self, value):
+            self.__y = value
