6ca1073103963ad1337110ce9abe2c70f8f00ced
root <Bbr011>
2024-03-06 07:45:59 -0800

    readit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Defines text file-reading function."""
+
+
 def read_file(filename=""):
+    """Print contents of UTF8 text file to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
-        f.read()
+        print(f.read(), end="")
