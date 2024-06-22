ddf58456de6a16cb84c7cc98db480111d663af60
Bbr011 <khadraouih7@gmail.com>
2024-06-22 11:55:16 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""ipt that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument v2"""
+"""script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument v2"""
 from sys import argv
 import MySQLdb
 
-if _name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=argv[3], charset="uft8")
     c = db.cusor()
     c.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC". (argv[4], ))
